d92b15698b777b73a585990a514c4a9c4335f513
Void321 <paulettz@icloud.com>
2022-08-10 17:27:35 +0900

    feat: roll dice and keep score feature

---
+++ script.js
@@ -0,0 +1,32 @@
+"use strict";
+//selecting elements
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+//starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
+
+let currentScore = 0;
+
+//Rolling functionality
+btnRoll.addEventListener("click", function () {
+  //1.Generating dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  //2. discplay dice
+  diceEl.classList.remove("hidden");
+  diceEl.src = `images/dice-${dice}.png`;
+
+  //3. Check for rolled 1
+  if (dice !== 1) {
+    //Add dice to current score
+    currentScore += dice;
+  } else {
+  }
+});
